8e532a62c1ae3844a77ddee3e8c16a82cc52c337
hojoon0724 <hojoon0724@gmail.com>
2024-07-15 15:45:16 -0700

    player split done with multiple arrays

--- scratch/playersArrayTest.js
+++ scratch/playersArrayTest.js
@@ -1,19 +1,32 @@
 let players = [['player[0]'], ['player[1]'], ['player[2]'], ['player[3]'], ['player[4]'], ['player[5]']];
 
 let playersWithCards = [
-  ['player[0]card[0]', 'player[0]card[1]'],
-  ['player[1]card[0]', 'player[1]card[1]'],
-  ['player[2]card[0]', 'player[2]card[1]'],
-  ['player[3]card[0]', 'player[3]card[1]'],
-  ['player[4]card[0]', 'player[4]card[1]'],
-  ['player[5]card[0]', 'player[5]card[1]'],
+  [
+    // ['player[0]stack[0]card[0]', 'player[0]stack[0]card[1]']
+  ],
+  [
+    ['player[1]stack[0]card[0]', 'player[1]stack[0]card[1]'],
+    ['player[1]stack[1]card[0]', 'player[1]stack[1]card[1]'],
+  ],
+  [
+    ['player[2]stack[0]card[0]', 'player[2]stack[0]card[1]'],
+    ['player[2]stack[1]card[0]', 'player[2]stack[1]card[1]'],
+  ],
+  [],
+  [
+    ['player[4]stack[0]card[0]', 'player[4]stack[0]card[1]'],
+    ['player[4]stack[1]card[0]', 'player[4]stack[1]card[1]'],
+    ['player[4]stack[2]card[0]', 'player[4]stack[2]card[1]'],
+  ],
+  [['player[5]stack[0]card[0]', 'player[5]stack[0]card[1]']],
 ];
 
 function printPlayerArray(array) {
-  for (i = 0; i < array.length; i++) {
-    console.log(array[i]);
-    for (j = 0; j < array[i].length; j++) {
-      console.log(array[i][j]);
+  for (player = 0; player < array.length; player++) {
+    if (array[player].length > 0) {
+      for (stack = 0; stack < array[player].length; stack++) {
+        console.log(array[player][stack], player, stack);
+      }
     }
   }
 }
